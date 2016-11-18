838acdd13b92fdcbc93b6ad3d00ab3d90238a3f0
valeriy.knyazhev <valeriy.knyazhev@yandex.ru>
2016-11-18 23:12:24 +0300

    added class Deanery

---
+++ src/main/java/com/fifteenthfloor/inplan/domain/model/Deanery.java
@@ -0,0 +1,17 @@
+package com.fifteenthfloor.inplan.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Valeriy Knyazhev valeriy.knyazhev@yandex.ru
+ */
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Deanery extends User {
+
+    private String label;
+
+}
