a3e12811b98816dc6dc224c6ed71d2ea3717f0c2
Valeriy Knyazhev <valeriy.knyazhev@bostongene.com>
2016-11-24 14:19:23 +0300

    добавлен интерфейся репозитория для профессоров и его реализация

---
+++ src/main/java/com/fifteenthfloor/inplan/port/adapter/InMemoryProfessorRepository.java
@@ -0,0 +1,47 @@
+package com.fifteenthfloor.inplan.port.adapter;
+
+import com.fifteenthfloor.inplan.domain.model.Professor;
+import com.fifteenthfloor.inplan.domain.model.ProfessorRepository;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Valeriy Knyazhev
+ */
+public class InMemoryProfessorRepository implements ProfessorRepository {
+
+    private Set<Professor> professors = new HashSet<> ();
+
+    public InMemoryProfessorRepository() {
+        loadProfessors();
+    }
+
+    private void loadProfessors() {
+        this.professors.add(new Professor());
+        this.professors.add(new Professor());
+    }
+
+    @Override
+    public ArrayList<Professor> getAllProfessors() {
+        return new ArrayList<>(this.professors);
+    }
+
+    @Override
+    public ArrayList<Professor> getProfessorsByLastName(String lastname) {
+        ArrayList<Professor> professors = new ArrayList<>();
+        for (Professor professor : this.professors) {
+            if (professor.getLastname().equals(lastname)) {
+                professors.add(professor);
+            }
+        }
+        return professors;
+    }
+
+    @Override
+    public Professor getProfessor(long id) {
+        return this.professors.stream().filter(pr -> id == pr.getId()).findFirst().orElse(null);
+    }
+
+}
