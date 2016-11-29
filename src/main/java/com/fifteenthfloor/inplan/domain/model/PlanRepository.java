f257efa80a6fc490f941f47950d2915a42644485
Valeriy Knyazhev <valeriy.knyazhev@yandex.ru>
2016-11-29 15:30:06 +0300

    добавлен интерфейс репозитория индивидуальных планов

---
+++ src/main/java/com/fifteenthfloor/inplan/domain/model/PlanRepository.java
@@ -0,0 +1,14 @@
+package com.fifteenthfloor.inplan.domain.model;
+
+import java.util.Set;
+
+/**
+ * @author Valeriy Knyazhev
+ */
+public interface PlanRepository {
+
+    Plan getPlan(long id);
+
+    Set<Long> getPlanCourses(long id);
+
+}
